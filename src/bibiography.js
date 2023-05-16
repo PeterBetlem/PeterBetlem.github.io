3517667ca5287b9698ff0a212c68fa5f4f61209e
Peter <Peter_Betlem@hotmail.com>
2023-05-16 11:53:13 +0200

    added educational/outreach from zotero.

--- src/bibiography.js
+++ src/bibiography.js
@@ -23,4 +23,32 @@ export function generateBibliography () {
     conferences.set(data)
 
     $('#conferences-list').html(conferences.format('bibliography', { format: 'html', template: 'geology', nosort: true }))
+
+    var chosenLibraryItems = "https://api.zotero.org/users/2084519/publications/items?format=csljson&limit=5&itemType=webpage&websiteType=JupyterBook/Sphinx&sort=date";
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', chosenLibraryItems, false);
+    xhr.send(null);
+    var citationData = JSON.parse(xhr.responseText);
+    console.log(citationData)
+    var data = citationData.items
+    var education = new Cite()
+
+    education.set(data)
+    console.log(education)
+
+    $('#education-list').html(education.format('bibliography', { format: 'html', template: 'geology', nosort: true }))
+
+    var chosenLibraryItems = "https://api.zotero.org/users/2084519/publications/items?format=csljson&limit=5&itemType=blogpost&sort=date";
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', chosenLibraryItems, false);
+    xhr.send(null);
+    var citationData = JSON.parse(xhr.responseText);
+    console.log(citationData)
+    var data = citationData.items
+    var popscience = new Cite()
+
+    popscience.set(data)
+    console.log(popscience)
+
+    $('#popscience-list').html(popscience.format('bibliography', { format: 'html', template: 'geology', nosort: true }))
 }
--- src/loadPages.js
+++ src/loadPages.js
@@ -4,7 +4,7 @@ export function loadSidebarPages () {
         $( "#home" ).load( "./pages/home.html" );
         $( "#projects" ).load( "./pages/projects.html" );
         $( "#contact" ).load( "./pages/contact.html" );
-        $( "#teaching" ).load( "./pages/teaching.html" );
+        //$( "#teaching" ).load( "./pages/teaching.html" );
         $( "#events" ).load( "./pages/events.html" );
         $( "#grants" ).load( "./pages/grants.html" )
         generateFundingPage2()
